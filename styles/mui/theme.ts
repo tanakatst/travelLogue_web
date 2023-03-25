be247796db11fa7ea79e29f57432da790c98db84
Sota Tanaka <sohtacareer12@gmail.com>
2023-03-26 00:42:55 +0900

    認証関連のUIが完成しました。

--- styles/mui/theme.ts
+++ styles/mui/theme.ts
@@ -1,74 +1,75 @@
-import { createTheme } from '@mui/material/styles';
-import Button from '@mui/material/Button';
+import { createTheme } from "@mui/material/styles";
+import Button from "@mui/material/Button";
 
 // mui theme typescript
-declare module '@mui/material/styles'{
-    interface Palette {
-        bgColor:{
-            secondaryWhite: BackgroundColor | undefined;
-            blue: string,
-            white: string,
-            charcoal: string,
-            orange:string
-        },
-        textColor:{
-            black:string,
-            white:string,
-            fullBlack:string,
-        }
-    }
-    interface PaletteOptions {
-        bgColor?:{
-            gray: string,
-            blue: string,
-            primaryWhite: string,
-            secondaryWhite:string,
-            charcoal: string,
-            orange:string
-        },
-        textColor?:{
-            black:string,
-            white:string,
-            fullBlack:string,
-        }
-    }
+declare module "@mui/material/styles" {
+  interface Palette {
+    bgColor: {
+      gray: string;
+      secondaryWhite: string;
+      blue: string;
+      white: string;
+      charcoal: string;
+      orange: string;
+      bladOrange: string;
+    };
+    textColor: {
+      black: string;
+      white: string;
+      fullBlack: string;
+    };
+  }
+  interface PaletteOptions {
+    bgColor?: {
+      gray: string;
+      blue: string;
+      primaryWhite: string;
+      secondaryWhite: string;
+      charcoal: string;
+      orange: string;
+      bladOrange: string;
+    };
+    textColor?: {
+      black: string;
+      white: string;
+      fullBlack: string;
+    };
+  }
 }
 
 const theme = createTheme({
-    // TODO:テーマ設定を行います
-    palette: {
-        bgColor:{
-            gray:'#6E6E6E',
-            blue:'#4A70A5',
-            primaryWhite:'#FFFFFF',
-            secondaryWhite: '#EDEDED',
-            charcoal: '#F4EADE',
-            orange:'#ED8C72'
-        },
-        textColor:{
-            black:'#585A5A',
-            white: '#EFECEC',
-            fullBlack: '#EFECEC'
-        }
+  // TODO:テーマ設定を行います
+  palette: {
+    bgColor: {
+      gray: "#6E6E6E",
+      blue: "#4A70A5",
+      primaryWhite: "#E6E6EB",
+      secondaryWhite: "#EDEDED",
+      charcoal: "#F4EADE",
+      orange: "#ED8C72",
+      bladOrange: "#E45934",
     },
-    typography:{
-        fontFamily: [
-            // 'DotGothic16,sans-serif',
-            '-apple-system',
-            'BlinkMacSystemFont',
-            '"Segoe UI"',
-            'Roboto',
-            '"Helvetica Neue"',
-            'Arial',
-            // 'sans-serif',
-            '"Apple Color Emoji"',
-            '"Segoe UI Emoji"',
-            '"Segoe UI Symbol"',
-          ].join(','),
+    textColor: {
+      black: "#585A5A",
+      white: "#EFECEC",
+      fullBlack: "#000",
     },
+  },
+  typography: {
+    fontFamily: [
+      // 'DotGothic16,sans-serif',
+      "-apple-system",
+      "BlinkMacSystemFont",
+      '"Segoe UI"',
+      "Roboto",
+      '"Helvetica Neue"',
+      "Arial",
+      // 'sans-serif',
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(","),
+  },
 });
 
 export default theme;
-
-
-
